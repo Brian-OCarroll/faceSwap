9aad571073cadb5b8a4154a73cefbff23d9d7feb
Brian-OCarroll <38870765+Brian-OCarroll@users.noreply.github.com>
2020-01-27 08:08:06 -0800

    Add files via upload

---
+++ faceSwap/server.js
@@ -0,0 +1,66 @@
+'use strict';
+var http = require('http');
+var port = process.env.PORT || 1337;
+var CloudmersiveImageApiClient = require('cloudmersive-image-api-client');
+var fs = require('fs');
+const { createCanvas,
+  loadImage, } = require('canvas');
+const sharp = require('sharp');
+
+// http.createServer(function (req, res) {
+// res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+var imageFile = Buffer.from(fs.readFileSync('./test.png').buffer);
+var face = Buffer.from(fs.readFileSync('./face.png').buffer);
+let image = sharp(imageFile)
+image.metadata()
+.then(metaData => {
+  return sharp
+  //925x
+  //925y
+})
+let faceImage = sharp(face)
+
+// loadImage(imageFile).then(image => {
+//    canvas = createCanvas(image.width, image.height);
+//   ctx = canvas.getContext('2d');
+//   console.log(image.width)
+//   console.log(image.height)
+//   ctx.drawImage(image, 0, 0, image.width, image.height)
+//   return loadImage('./face.png')
+// }).then(image => {
+//   ctx.drawImage(image, 37, 65 , 125, 124)
+// }).then(() => {
+//   var buf = canvas.toBuffer();
+// fs.writeFileSync("return.png", buf);
+// })
+//   .catch(err => {
+//     console.log(err)
+//   });
+
+
+var defaultClient = CloudmersiveImageApiClient.ApiClient.instance;
+
+// Configure API key authorization: Apikey
+var Apikey = defaultClient.authentications['Apikey'];
+Apikey.apiKey = "50d00d85-4681-4c27-999c-1f4b5327d1e3" // Get your own key at https://account.cloudmersive.com
+
+
+
+var apiInstance = new CloudmersiveImageApiClient.FaceApi()
+
+
+
+
+var callback = function (error, data, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+  }
+};
+
+apiInstance.faceLocateWithLandmarks(imageFile, callback);
+
+
+// }).listen(port);
